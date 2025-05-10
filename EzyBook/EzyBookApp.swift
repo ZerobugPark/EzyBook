bf47515b6e9875b6a19273a389a8499fd75fc4b1
young <zerobugpark@gmail.com>
2025-05-10 21:22:37 +0900

    Initial Commit

---
+++ EzyBook/EzyBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  EzyBookApp.swift
+//  EzyBook
+//
+//  Created by youngkyun park on 5/10/25.
+//
+
+import SwiftUI
+
+@main
+struct EzyBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
